d2b9d226271797304ef547a2d143c98436885bf6
Mekuanint Abebe <se.mekuanint.abebe@gmail.com>
2021-02-17 23:15:37 +0300

    final

--- ImageGallary/assets/js/slider.js
+++ ImageGallary/assets/js/slider.js
@@ -1,29 +1,31 @@
-const activeSlide = document.querySelector(".carousel-inner")
+const slidersBtns = document.querySelector(".carousel-inner")
 const indicators = document.querySelector(".carousel-indicators")
 document.addEventListener("DOMContentLoaded", (e) => {
-    // open the promise
+    // open the request from the api
     fetch("https://picsum.photos/v2/list?page=2&limit=8")
-    //return the JSON Promise
+    // return the json Promise
         .then(resolve => resolve.json())
-        .then(returnedData => {
-            for (let i = 0; i < returnedData.length; i++) {
-                if (i == 0) {
-                    activeSlide.innerHTML += `
+        .then(fetchedData => {
+            for (let i = 0; i < fetchedData.length; i++) {
+                if (i === 0) {
+                    slidersBtns.innerHTML += `
                     <div class="carousel-item active">
-                        <img class="d-block w-100" src="${data[i].download_url}" alt="First slide">
+                        <img class="d-block w-100" src="${fetchedData[i].download_url}" alt="First slide">
                     </div>
                     `
-                    indicators.innerHTML += `<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>`
+                    indicators.innerHTML += `<li fetchedData-target="#carouselExampleIndicators" fetchedData-slide-to="0" class="active"></li>`
                 }
                 else {
-                    activeSlide.innerHTML += `
+                    slidersBtns.innerHTML += `
                     <div class="carousel-item">
-                        <img class="d-block w-100" src="${data[i].download_url}" alt="First slide">
+                        <img class="d-block w-100" src="${fetchedData[i].download_url}" alt="Other slides">
                     </div>`
-                    indicators.innerHTML += `<li data-target="#carouselExampleIndicators" data-slide-to="${i}"></li>`
+                    indicators.innerHTML += `<li fetchedData-target="#carouselExampleIndicators" fetchedData-slide-to="${i}"></li>`
                 }
             }
             return 1
         })
-
+        .catch(function(err) {
+            console.log(err);
+        });
 })
